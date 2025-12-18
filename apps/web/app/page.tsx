5d5bc9a85b505b08c46757de953dac2f4dd0ec55
Theryston Santos <devtheryston@gmail.com>
2025-12-18 10:13:02 -0300

    install dialog to ui

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,11 +1,29 @@
 import { Button } from "@iconoma/ui/components/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@iconoma/ui/components/dialog";
 
 export default function Page() {
   return (
     <div className="flex items-center justify-center min-h-svh">
       <div className="flex flex-col items-center justify-center gap-4">
         <h1 className="text-2xl font-bold">Hello World</h1>
-        <Button size="sm">Button</Button>
+        <Dialog>
+          <DialogTrigger asChild>
+            <Button size="sm">Open Dialog</Button>
+          </DialogTrigger>
+          <DialogContent>
+            <DialogHeader>
+              <DialogTitle>Dialog Title</DialogTitle>
+              <DialogDescription>Dialog Description</DialogDescription>
+            </DialogHeader>
+          </DialogContent>
+        </Dialog>
       </div>
     </div>
   );
