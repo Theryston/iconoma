5d245854aed58a9b2e486ef49e188375f6fab73e
Theryston Santos <devtheryston@gmail.com>
2025-12-20 10:41:21 -0300

    refactor: simplify header component by removing SearchInput and restructuring layout for improved clarity

--- apps/studio/src/components/header/index.tsx
+++ apps/studio/src/components/header/index.tsx
@@ -1,10 +1,8 @@
 import { Button } from "@iconoma/ui/components/button";
-import { Separator } from "@iconoma/ui/components/separator";
 import { CommitButton } from "./commit-button";
 import { PlusIcon } from "lucide-react";
 import { ConfigButton } from "./config-button";
 import { Link } from "react-router";
-import { SearchInput } from "./search-input";
 import { ColorPicker } from "./color-picker";
 
 export function Header() {
@@ -12,23 +10,13 @@ export function Header() {
     <>
       <div className="h-16" />
       <header className="flex items-center justify-between p-4 w-full border-b fixed top-0 left-0 right-0 z-50 h-16 bg-background shadow-sm">
-        <div className="grid grid-cols-[auto_auto_1fr] items-center gap-4 w-fit">
-          <Link
-            to="/"
-            className="hover:opacity-80 transition-opacity flex items-center gap-2 w-full"
-          >
-            <img src="/icon.png" alt="Iconoma Logo" className="w-6 h-6" />
-            <span className="text-sm font-medium">Iconoma Studio</span>
-          </Link>
-
-          <div className="h-6">
-            <Separator orientation="vertical" />
-          </div>
-
-          <div className="w-full">
-            <SearchInput />
-          </div>
-        </div>
+        <Link
+          to="/"
+          className="hover:opacity-80 transition-opacity flex items-center gap-2 w-full"
+        >
+          <img src="/icon.png" alt="Iconoma Logo" className="w-6 h-6" />
+          <span className="text-sm font-medium">Iconoma Studio</span>
+        </Link>
 
         <div className="flex items-center gap-2">
           <ColorPicker />
--- apps/studio/src/components/header/search-input.tsx
+++
@@ -1,5 +0,0 @@
-import { Input } from "@iconoma/ui/components/input";
-
-export function SearchInput() {
-  return <Input type="text" placeholder="Search" className="w-80" />;
-}
