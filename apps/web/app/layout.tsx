ba34d07f0383a4a76bd3c5f2409b4946efe81404
Theryston Santos <devtheryston@gmail.com>
2025-12-21 12:38:11 -0300

    feat: add metadata for improved SEO and page description

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -2,6 +2,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 
 import "@iconoma/ui/globals.css";
 import { Providers } from "@/components/providers";
+import { Metadata } from "next";
 
 const fontSans = Geist({
   subsets: ["latin"],
@@ -12,6 +13,12 @@ const fontMono = Geist_Mono({
   subsets: ["latin"],
   variable: "--font-mono",
 });
+
+export const metadata: Metadata = {
+  title: "Iconoma - The best way to standardize icons and SVGs",
+  description:
+    "Iconoma is a complete icon management system that helps teams organize, standardize, version, and distribute icons with a reliable workflow. Turn your scattered SVG files into a structured, reproducible pipeline.",
+};
 
 export default function RootLayout({
   children,
--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import Image from "next/image";
 import { Button } from "@iconoma/ui/components/button";
 import { Badge } from "@iconoma/ui/components/badge";
 import {
