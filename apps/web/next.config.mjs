65a0ec6cafad02565b8198fd26aa1e1cae1bd6eb
Theryston Santos <devtheryston@gmail.com>
2026-09-11 09:34:10 -0300

    fly.io deploy

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "standalone",
   transpilePackages: ["@iconoma/ui"],
-}
+};
 
-export default nextConfig
+export default nextConfig;
